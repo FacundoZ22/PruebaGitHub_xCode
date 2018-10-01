5c3c61d11dfc3338bcd210fe50d87e467bb98140
Laboratorio 10 <laboratoriounam-apple10@macbook-pro-de-laboratorio-10.local>
2018-10-01 14:00:29 -0500

    Initial Commit

---
+++ Octubre1ero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Octubre1ero
+//
+//  Created by Laboratorio UNAM-Apple 10 on 01/10/18.
+//  Copyright © 2018 FCA.UNAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
